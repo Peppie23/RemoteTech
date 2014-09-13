531fa000a00823e5d972ffee8bbbd22debaf628f
Dennis <peppie23@gmail.com>
2014-09-13 21:44:22 +0200

    License Test 1

--- CI/github.build.deploy.sh
+++ CI/github.build.deploy.sh
@@ -38,7 +38,7 @@ then
 fi
 
 echo "Copy the licence to the game data folder"
-wget "https://github.com/RemoteTechnologiesGroup/RemoteTech/blob/master/LICENSE.txt" > GameData/LICENSE.txt
+wget -O "GameData/RemoteTech2/LICENSE.txt" "https://github.com/RemoteTechnologiesGroup/RemoteTech/blob/master/LICENSE.txt"
 
 echo "Build ${TRAVIS_BUILD_NUMBER} from branch ${TRAVIS_BRANCH} in ${TRAVIS_REPO_SLUG}" > GameData/build.txt
 echo "Built from commit ${TRAVIS_COMMIT} with tag ${BUILDTAG}" >>  GameData/build.txt
